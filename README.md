2cf8800d6d36b16de86fa212d3e0eaba92c26fec
rmorapreciado <134554465+rmorapreciado@users.noreply.github.com>
2023-05-25 05:27:37 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ProyectoPokemon
+# SegundoProyectoCientifica
--- app.js
+++ app.js
@@ -1,53 +1,88 @@
-const URLListDePokemones = "https://pokeapi.co/api/v2/pokemon"; // La línea de código en JavaScript define una constante llamada "URLListaDePokemones" que almacena la dirección URL de una API web que proporciona datos de Pokemones.  La URL se utiliza más tarde en la aplicación para hacer una solicitud HTTP a la API web y obtener los datos de Pokemones que se utilizarán en la aplicación.
-const nodePokemonList = "list-pokemon";
-const nodePokemonName = "name-pokemon";
-const nodePokemonImg = "img-pokemon";
-const nodePokemonMoves = "moves-pokemon";
+const resultsCalc = document.querySelector("#results_calc");
+const showResult = document.querySelector('.show-result');
+let showOp = document.querySelector('#texto');
 
-function showPokemon(pokemon) {
-    let namePokemon = document.getElementById(nodePokemonName);
-    let imgPokemon = document.getElementById(nodePokemonImg);
-    let movesPokemon = document.getElementById(nodePokemonMoves);
-    namePokemon.innerHTML = pokemon.name;
-    imgPokemon.src = pokemon.sprites.front_default;
-    movesPokemon.url = pokemon.moves.move.swords - dance;
-}
+const onOffButton = document.querySelector('.onOff');
 
-function obtainPokemon(event) {
-    event.preventDefault();
-    let URLDePokemon = event.target.href;
-    console.log(URLDePokemon);
-    fetch(URLDePokemon).then(function(response) {
-        return response.json();
-    }).then(function(data) {
-        showPokemon(data);
-        console.log(data);
+onOffButton.addEventListener('click', () => {
+    resultsCalc.value = '';
+    showResult.textContent = '';
+    showOp.classList.toggle('turn-on-off');
+});
+
+function calculator() {
+    document.addEventListener('click', e => {
+        let target = e.target;
+        if (target.tagName === "BUTTON") {
+            let cutedValue = target.value.trim();
+            switch (true) {
+                case cutedValue == "equal":
+                    checkButtonValue(resultsCalc) ? printResult(resultsCalc) : 'ERROR SINTAXIS';
+                    break;
+                case cutedValue == "clear":
+                    resultsCalc.value = '';
+                    showResult.textContent = '';
+                    showOp.textContent = '';
+                    break;
+                case cutedValue == "del":
+                    deleteCharacter(resultsCalc);
+                    showOp.textContent = showOp.textContent.slice(0, -1);
+                    break;
+                case (cutedValue === "ln" ||
+                    cutedValue === "exp" ||
+                    cutedValue === "pow" ||
+                    cutedValue === "log" ||
+                    cutedValue === "ans"
+
+                    ||
+                    cutedValue === "sq" ||
+                    cutedValue === "sqrt" ||
+                    cutedValue === "sin" ||
+                    cutedValue === "cos" ||
+                    cutedValue === "tan"):
+                    if (checkButtonValue(resultsCalc)) {
+                        if (cutedValue === "ln") {
+                            cutedValue = "log";
+                        } else if (cutedValue === "pow") {
+                            resultsCalc.value = eval(resultsCalc.value) * eval(resultsCalc.value) * eval(resultsCalc.value);
+                            showOp.textContent = resultsCalc.value
+                            break;
+                        } else if (cutedValue === "sq") {
+                            resultsCalc.value = eval(resultsCalc.value) * eval(resultsCalc.value);
+                            showOp.textContent = resultsCalc.value
+                            break;
+                        }
+                        resultsCalc.value = eval(Math[cutedValue](resultsCalc.value));
+                        showOp.textContent = resultsCalc.value
+                    }
+                    break;
+                default:
+                    addDisplay(resultsCalc, target.value);
+                    break;
+            }
+        }
     });
 }
-
-function showPokemones(listDePokemones) {
-    let listPokemon = document.getElementById(nodePokemonList); // obtnemos el nodo de la lista que es ol y que tiene de indentificador lista-pokemon
-    for (let i = 0; i < listDePokemones.length; i++) { // La línea de código es una estructura de control de bucle "for" en JavaScript que se utiliza para recorrer una matriz o una lista de elementos, ejecutando una acción para cada uno de ellos.
-        let pokemon = listDePokemones[i].name; // Sirve para extraer el nombre de cada objeto "pokemon" de una lista o matriz de objetos "listaDePokemones".
-        let URLPokemon = listDePokemones[i].url;
-        let name = document.createTextNode(pokemon); // Esta línea de código sirve para una creación de nodo de texto en JavaScript que se utiliza para crear un nuevo nodo de texto en el árbol de DOM y asignarle un valor. En resumen, esta línea de código se utiliza para crear un nuevo nodo de texto en el árbol de DOM en JavaScript y asignarle un valor. 
-
-        let a = document.createElement("a");
-        let li = document.createElement("li"); // Esta línea de código es una creación de elemento en JavaScript que se utiliza para crear un nuevo elemento HTML "li" (elemento de lista) en el árbol de DOM.
-        a.appendChild(name);
-        a.setAttribute("href", URLPokemon);
-        a.addEventListener("click", obtainPokemon);
-        li.appendChild(a);
-        listPokemon.appendChild(li);
+const addDisplay = (input, character) => {
+    showOp.textContent = input.value + character.trim();
+    input.value == null || input.value == "" ?
+        input.value = character.trim() :
+        input.value += character.trim();
+}
+const deleteCharacter = (button) => {
+    button.value = button.value.substring(0, button.value.length - 1)
+}
+const printResult = (calc) => {
+    showResult.textContent = eval(calc.value)
+}
+const checkButtonValue = (string) => {
+    for (let i = 0; i < string.length; i++) {
+        let character = string[i];
+        if (!["/", "*", "+", "-", ".", "(", ")"].includes(character) && isNaN(character)) {
+            alert("invalid entry!");
+            return false;
+        }
     }
-}
-
-function obtainListDePokemones() {
-    fetch(URLListDePokemones).then(function(response) {
-        return response.json();
-    }).then(function(data) {
-        showPokemones(data.results);
-    });
-}
-
-obtainListDePokemones();
+    return true;
+};
+calculator();
